8e92e0f0e4e264b0039965d65aab6e7274ea3b1e
lyucean <lyucean@gmail.com>
2020-04-27 03:01:11 +0300

    clean rep.

--- zipper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-wget https://github.com/botman/studio/archive/master.zip
-unzip master.zip -d working
-cd working/studio-master
-zip -ry ../../latest.zip .
-cd ../..
-rm -rf working
-rm master.zip
