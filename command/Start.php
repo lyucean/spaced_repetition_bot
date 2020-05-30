ab6be442ca49fafa9f8dd0d7c2b0db0eaeb285d9
lyucean <lyucean@gmail.com>
2020-05-30 22:40:50 +0300

    started writing an routing

---
+++ command/Start.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace srbot\command;
+
+use srbot\core\Model;
+
+class Start extends Model
+{
+    public function index()
+    {
+    }
+}
--- core/Routing.php
+++ core/Routing.php
@@ -3,21 +3,57 @@
 
 namespace srbot\core;
 
-
+/**
+ * Class Routing
+ * @package srbot\core
+ * @example https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+ * @example https://github.com/steampixel/simplePHPRouter/tree/master
+ */
 class Routing
 {
-    public function index($text)
+    const BASE_DIR = '../command/';
+
+    public function index($class, $arg)
     {
         // If it's a callback, we'll redirect it to the last command.
-        /*        if ($this->telegram->getUpdateType() == 'callback_query') {
-                    // Get the last command called
 
-                    return;
-                }*/
 
         // If it's an independent command.
         if (mb_substr($text, 0, 1, 'UTF-8') == '/') {
-            return;
         }
+        // This is command or callback
+        if ($this->telegram->getUpdateType() == 'callback_query') {
+            // Get the last command called
+
+            return;
+
+            return;
+        } else {
+        }
+//
+//                $fn = 'pages/admin.php';
+//
+//                if (file_exists($fn))
+//                    reqiure($fn);
+//                else
+//                    reqiure('pages/404.php');
+
+    }
+
+    /**
+     * If a file exists, require it from the file system.
+     *
+     * @param string $file The file to require.
+     * @return bool True if the file exists, false if not.
+     */
+    public function requireFile($file)
+    {
+        $path = self::BASE_DIR . $file . '.php';
+
+        if (file_exists($path)) {
+            require $path;
+            return true;
+        }
+        return false;
     }
 }
