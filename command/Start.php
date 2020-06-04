8d50b51788767142621ee71dc851a4afe6e7b11c
lyucean <lyucean@gmail.com>
2020-06-04 14:05:35 +0300

    + command \start

--- command/Start.php
+++ command/Start.php
@@ -4,8 +4,20 @@ namespace srbot\command;
 
 class Start
 {
+    private $telegram;
+    private $chat_id;
+
+    public function __construct($telegram)
+    {
+        $this->telegram = $telegram;
+        $this->chat_id = $this->telegram->ChatID();
+    }
+
     public function index()
     {
-        echo null;
+        $reply = "Hey, the bot is in development, it's too early to use it :) \n
+                  I will write to you how it will be ready.";
+        $content = ['chat_id' => $this->chat_id, 'text' => $reply];
+        $this->telegram->sendMessage($content);
     }
 }
