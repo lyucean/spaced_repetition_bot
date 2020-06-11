279a69612f6cb2032c4690db5565cf893a6ddaba
lyucean <lyucean@gmail.com>
2020-06-11 15:55:11 +0300

    Added the ability to delete content

--- command/Content.php
+++ command/Content.php
@@ -31,7 +31,7 @@ class Content
     public function add()
     {
         if ('message' != $this->telegram->getUpdateType()) {
-            (new Error($this->telegram))->send('This is not a message!', false);
+            (new Error($this->telegram))->send('This is not a message.', false);
             return;
         }
 
@@ -52,14 +52,13 @@ class Content
                     $url = '',
                     '/content/cancel?content_id=' . $this->content_id
                 ),
-                //$this->telegram->buildInlineKeyBoardButton('Menu', $url = '', $callback_data = '/menu'),
             ],
         ];
 
         $content = [
             'chat_id' => $this->chat_id,
             'reply_markup' => $this->telegram->buildInlineKeyBoard($option),
-            'text' => 'I saved it!'
+            'text' => 'I saved it.'
         ];
         $this->telegram->sendMessage($content);
     }
@@ -67,22 +66,22 @@ class Content
     public function cancel()
     {
         if ('callback_query' != $this->telegram->getUpdateType()) {
-            (new Error($this->telegram))->send('This is not a callback query!', false);
+            (new Error($this->telegram))->send('This is not a callback query.', false);
             return;
         }
 
         $param = get_var_query($this->telegram->Text());
 
         if (empty($param['content_id'])) {
-            (new Error($this->telegram))->send('I did not find content_id!');
+            (new Error($this->telegram))->send('I did not find content.');
         }
 
         $this->content_id = $param['content_id'];
 
-        $reply = 'Deleted! ID:' . $this->content_id;
+        $reply = 'Deleted.';
 
         if (!$this->db->deleteContent($this->content_id)) {
-            $reply = 'This content has already been removed!';
+            $reply = 'This content has already been removed.';
         }
 
         $content = ['chat_id' => $this->chat_id, 'text' => $reply];
@@ -97,17 +96,29 @@ class Content
     {
         $content = $this->db->getContentPrepared($this->chat_id);
 
-        if (!empty($content)) {
-            $this->telegram->sendMessage(
-                [
-                    'chat_id' => $this->chat_id,
-                    'text' => $content['text']
-                ]
-            );
-
-            $this->db->addDateReminderContent($content['content_id']);
+        if (empty($content)) { // If there is nothing to send
+            return;
         }
+
+        // Add the information that we have already shown this content
+        $this->db->addDateReminderContent($content['content_id']);
+
+        $option = [
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    'Delete this',
+                    $url = '',
+                    '/content/cancel?content_id=' . $content['content_id']
+                ),
+            ],
+        ];
+
+        $this->telegram->sendMessage(
+            [
+                'chat_id' => $this->chat_id,
+                'reply_markup' => $this->telegram->buildInlineKeyBoard($option),
+                'text' => $content['text']
+            ]
+        );
     }
-
-
 }
