fede76984ef837336012fedd92bad004f2ced2a3
lyucean <lyucean@gmail.com>
2020-06-25 15:18:20 +0300

    error tracking only PROD

--- app/tracking.php
+++ app/tracking.php
@@ -1,17 +1,19 @@
 <?php
 
-$notifier = new Airbrake\Notifier(
-    array(
-        'projectId' => AIR_BRAKE_PROJECT_ID,
-        "environment" => ENVIRONMENT,
-        'projectKey' => AIR_BRAKE_PROJECT_KEY
-    )
-);
+if (OC_ENV_PROD) {
+    $notifier = new Airbrake\Notifier(
+        array(
+            'projectId' => AIR_BRAKE_PROJECT_ID,
+            "environment" => ENVIRONMENT,
+            'projectKey' => AIR_BRAKE_PROJECT_KEY
+        )
+    );
 
-Airbrake\Instance::set($notifier);
+    Airbrake\Instance::set($notifier);
 
-$handler = new Airbrake\ErrorHandler($notifier);
-$handler->register();
+    $handler = new Airbrake\ErrorHandler($notifier);
+    $handler->register();
+}
 
 //try {
 //    throw new Exception('Test');
